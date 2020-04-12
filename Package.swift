3e10ab5542214d318f949e8ec697bde2a938424f
Daniel Kirstenpfad <bietiekay@gmail.com>
2020-04-12 11:14:30 +0200

    another version bump

--- Package.swift
+++ Package.swift
@@ -7,13 +7,13 @@ let package = Package(
         .library(name: "SwiftOpenStreetMap", targets: ["SwiftOpenStreetMap"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/antitypical/Result.git", .upToNextMinor(from: "4.0.0")),
+        .package(url: "https://github.com/antitypical/Result.git", .upToNextMinor(from: "4.0.1")),
         .package(url: "https://github.com/cbguder/CBGPromise.git", .upToNextMinor(from: "0.5.0")),
-        .package(url: "https://github.com/younata/FutureHTTP.git", .upToNextMinor(from: "0.1.8")),
+        .package(url: "https://github.com/younata/FutureHTTP.git", .upToNextMinor(from: "1.2.0")),
         .package(url: "https://github.com/swiftyjson/SwiftyJSON.git", .upToNextMinor(from: "5.0.0")),
 
-        .package(url: "https://github.com/quick/Quick.git", .upToNextMinor(from: "1.3.2")),
-        .package(url: "https://github.com/quick/Nimble.git", .upToNextMinor(from: "7.3.1"))
+        .package(url: "https://github.com/quick/Quick.git", .upToNextMinor(from: "1.3.4")),
+        .package(url: "https://github.com/quick/Nimble.git", .upToNextMinor(from: "7.3.4"))
     ],
     targets: [
         .target(name: "SwiftOpenStreetMap", dependencies: ["Result", "CBGPromise", "FutureHTTP", "SwiftyJSON"]),
